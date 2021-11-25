95969a7420d53ab0deb742f0895d9bb3726222e6
Osprey99201 <saadbinosama@gmail.com>
2021-11-25 22:32:04 +0430

    local Storage

--- app.js
+++ app.js
@@ -1,46 +1,66 @@
 //Book Class: represents a Book
-class Book{
+class Book {
     constructor(title, author, isbn) {
-this.title = title;
-this.author = author;
-this.isbn = isbn;
- }
-}
-
-//UI Class; Handle UI Tasks 
-class UI{
-static displayBooks() {
-    const StoredBooks = [
-        {
-    title: 'Book One',
-    author: 'jhon doe',
-     title:'344334'
-        },
-    {
-        title: 'Book Two',
-        author: 'Jane doe',
-        isbn: '5555'
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
     }
-   ];
-
-   const books = StoredBooks;
-
-   books.forEach((book) => UI.addBookTol=List(book));
   }
 
-  static addBookTolist(book) {
-const list = document.querySelector('#book-list');
+//UI Class; Handle UI Tasks 
+class UI {
+    static displayBooks() {
+        const books = Store.getBooks();
+    
+        books.forEach((book) => UI.addBookToList(book));
+      }
+    
+      static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+    
+        const row = document.createElement('tr');
+    
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.isbn}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+    
+        list.appendChild(row);
+      }
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'jhon doe',
+                isbn: '344334'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane doe',
+                isbn: '5555'
+            }
+        ];
 
-const row = document.createElement('tr');
+        const books = StoredBooks;
 
-row.innerHTML= `
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+        static addBookTolist(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
 <td>${book.title}</td>
 <td>${book.author}</td>
 <td>${book.isbn}</td>
 <td><a href="#" class="btn btn-danger btn-sm delete>X</a></td>
 `;
-list.appendChild(row);
-  }
+        list.appendChild(row);
+    }
 }
 
 //Store Class: Handles storage
